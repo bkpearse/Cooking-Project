4d3bd99b0231151076f26a9695813bc6ea186309
Vincent Timperio <vincent.timperio@gmail.com>
2023-03-28 19:32:22 +0200

    Import of unused function removed.

--- project_cook/api_folder/api_file.py
+++ project_cook/api_folder/api_file.py
@@ -1,7 +1,6 @@
 from fastapi import Body, FastAPI, File, UploadFile
 
-from project_cook.interface.main import (pred_streamlit, predict_function,
-                                         search_recipes)
+from project_cook.interface.main import pred_streamlit, search_recipes
 
 api = FastAPI()
 
@@ -10,30 +9,6 @@ api = FastAPI()
 @api.get("/")
 def index():
     return {"ok": "API connected"}
-
-
-@api.get("/predict")
-def predict(feature1, feature2):
-
-    # model = picle.load_model()
-    # prediction = model.predict(feature1, feature2)
-
-    # Here, I'm only returning the features, since I don't actually have a model.
-    # In a real life setting, you would return the predictions.
-
-    return {'prediction': int(feature1) * int(feature2)}
-
-
-# @api.post("/what-to-eat")
-# async def what_to_eat(file: UploadFile = File(...)):
-#     filepath = 'notebooks/images/' + file.filename[:-4]
-#     results = []
-#     async with aiofiles.open(filepath+'/'+file.filename, 'wb') as out_file:
-#         content = await file.read()  # async read
-#         await out_file.write(content)  # async write
-#         results = predict_function(Path(filepath))
-#     return {"Result": results}
-#     # return {'file': file, 'content': contents}
 
 
 @api.get("/query-recipes/{lang}")
